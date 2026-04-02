cd7be05b809376f2c1337c3b10f61ef3d943d9d7
bajvoc <vladimir.padha@gmail.com>
2026-04-02 10:16:54 +0200

    Fix: Folder fix
    Folder fix

--- music_assistant/providers/webdav/__init__.py
+++ music_assistant/providers/webdav/__init__.py
@@ -199,9 +199,8 @@ class WebDavProvider(MusicProvider):
                 # Skip the 'current' and 'parent' directory markers
                 if filename not in (".", "..", "./", "../") and not filename.startswith("."):
                     self.logger.debug(f"Found file: {filename}")
-                    directory = await asyncio.to_thread(
-                        self._client.is_dir, f"{filename.rstrip('/')}"
-                    )
+                    # directory = filename.endswith("/")
+                    directory = await asyncio.to_thread(self._client.is_dir, filename.rstrip("/"))
 
                     if directory:
                         items.append(
