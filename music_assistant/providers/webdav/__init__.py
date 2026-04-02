345e9e2aa3d14cc45b1c46b5da07223c9c11a4d6
bajvoc <vladimir.padha@gmail.com>
2026-04-02 09:55:54 +0200

    Fix: Filter  out http
    Filter  out http

--- music_assistant/providers/webdav/__init__.py
+++ music_assistant/providers/webdav/__init__.py
@@ -185,7 +185,7 @@ class WebDavProvider(MusicProvider):
         # Use the library to get a list of files/folders
         # item_id is usually the relative path
         # Normalize path
-        if path and path.startswith("webdav://"):
+        if path and path.startswith("webdav://") and not path.contains("http"):
             current_path = path.replace("webdav://", "", 1)
             self.logger.debug(f"Browsing path: {current_path}")
             try:
