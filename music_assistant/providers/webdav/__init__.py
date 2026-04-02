52a73341269c5fe26dbb90225f17774c83e89688
bajvoc <vladimir.padha@gmail.com>
2026-04-02 11:04:51 +0200

    Fix: StreamDetails
    content_type added

--- music_assistant/providers/webdav/__init__.py
+++ music_assistant/providers/webdav/__init__.py
@@ -298,7 +298,6 @@ class WebDavProvider(MusicProvider):
 
     async def get_stream_details(self, item_id: str, media_type: MediaType) -> StreamDetails:
         """Still use direct_url for the actual playback."""
-        # content_type=ContentType.UNKNOWN, # MASS will auto-detect from the stream
         return StreamDetails(
             provider=self.domain,
             item_id=item_id,
@@ -306,6 +305,7 @@ class WebDavProvider(MusicProvider):
             stream_type=StreamType.HTTP,
             path=f"{self.config.get_value('url')}/{item_id}",
             can_seek=True,
+            content_type=ContentType.UNKNOWN,
         )
 
     # async def get_artist_albums(self, prov_artist_id: str) -> list[Album]:  # type: ignore[empty-body]
