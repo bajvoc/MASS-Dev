225b28f06ffcdb27b98268fac08fa3adf1532738
bajvoc <vladimir.padha@gmail.com>
2026-04-04 02:10:55 +0200

    Fix: get_album_tracks
    get_album_tracks

--- music_assistant/providers/webdav/__init__.py
+++ music_assistant/providers/webdav/__init__.py
@@ -337,6 +337,7 @@ class WebDavProvider(MusicProvider):
                 if item.lower().endswith((".mp3", ".flac", ".wav", ".m4a")):
                     track_path = f"{album_path.rstrip('/')}/{item.lstrip('/')}"
                     track_obj = await self._get_track_metadata(track_path)
+                    self.logger.debug(f"get_album_tracks: title: {track_obj.title}")
                     tracks.append(track_obj)
 
         except Exception as err:
